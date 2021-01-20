048168c5489839c566e0e6d385201817483d4e9c
lizardkingLK <53265303+lizardkingLK@users.noreply.github.com>
2021-01-20 19:20:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./styles.css";
+
+import Navbar from "./components/Navbar";
+import Booklist from "./components/Booklist";
+
+import ThemeContextProvider from "./contexts/ThemeContext";
+import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./contexts/AuthContext";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+        <AuthContextProvider>
+          <Navbar />
+          <Booklist />
+          <ThemeToggle />
+        </AuthContextProvider>
+      </ThemeContextProvider>
+    </div>
+  );
+}
